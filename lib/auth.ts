ca29ecc6aaca3ef7fb3c296d7ff7c616c41a2712
TomBirbeck <tombirbeck47@gmail.com>
2023-01-09 00:41:54 +0000

    added create jwt and validate jwt functions as well as jwt secret in env

---
+++ lib/auth.ts
@@ -0,0 +1,31 @@
+import bcyrpt from 'bcrypt';
+import { SignJWT, jwtVerify } from 'jose';
+import { db } from './db';
+
+export const hashPassword = (password: string) => bcyrpt.hash(password, 10);
+
+export const comparePasswords = (
+  plainPassword: string,
+  hashedPassword: string
+) => bcyrpt.compare(plainPassword, hashedPassword);
+
+export const createJWT = (user) => {
+  const iat = Math.floor(Date.now() / 1000);
+  const exp = iat + 60 * 60 * 24 * 7;
+
+  return new SignJWT({ payload: { id: user.id, email: user.email } })
+    .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+    .setExpirationTime(exp)
+    .setIssuedAt(iat)
+    .setNotBefore(iat)
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET));
+};
+
+export const validateJWT = async (jwt) => {
+  const { payload } = await jwtVerify(
+    jwt,
+    new TextEncoder().encode(process.env.JWT_SECRET)
+  );
+
+  return payload.payload as any;
+};
