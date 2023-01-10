03930bffa040eb2bac2da602e8ec7ca7156b0dd8
TomBirbeck <tombirbeck47@gmail.com>
2023-01-10 09:44:50 +0000

    added new project component and added it to home page

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,13 +1,14 @@
-import { delay } from "@/lib/delay";
-import { getUserFromCookie } from "@/lib/auth";
-import { db } from "@/lib/db";
-import { cookies } from "next/headers";
-import Link from "next/link";
-import { Suspense } from "react";
-import Greeting from "@/components/Greeting";
-import GreetingsSkeleton from "@/components/GreetingsSkeleton";
-import ProjectCard from "@/components/ProjectCard";
-import TaskCard from "@/components/TaskCard";
+import { delay } from '@/lib/delay';
+import { getUserFromCookie } from '@/lib/auth';
+import { db } from '@/lib/db';
+import { cookies } from 'next/headers';
+import Link from 'next/link';
+import { Suspense } from 'react';
+import Greeting from '@/components/Greeting';
+import GreetingsSkeleton from '@/components/GreetingsSkeleton';
+import ProjectCard from '@/components/ProjectCard';
+import TaskCard from '@/components/TaskCard';
+import NewProject from '@/components/NewProject';
 
 const getData = async () => {
   await delay(2000);
@@ -24,32 +25,34 @@ const getData = async () => {
   return { projects };
 };
 
-
 export default async function Page() {
-  const {projects} = await getData()
+  const { projects } = await getData();
   return (
-    <div className="h-full overflow-y-auto pr-6 w-full">
-    <div className=" h-full items-stretch justify-center min-h-[content]">
-      <div className="flex-1 grow flex">
-        <Suspense fallback={<GreetingsSkeleton />}>
-          <Greeting />
-        </Suspense>
-      </div>
-      <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
-        {projects.map(project => (
-          <div className="w-1/3 p-3" key={project.id}>
-            <Link href={`/project/${project.id}`}>
-              <ProjectCard project={project}/>
-            </Link>
-          </div>
-        ))}
-          <div className="w-1/3 p-3">{/* new project here */}</div>
+    <div className='h-full overflow-y-auto pr-6 w-full'>
+      <div className=' h-full items-stretch justify-center min-h-[content]'>
+        <div className='flex-1 grow flex'>
+          <Suspense fallback={<GreetingsSkeleton />}>
+            <Greeting />
+          </Suspense>
         </div>
-        <div className="mt-6 flex-2 grow w-full flex">
-          <div className="w-full"><TaskCard /></div>
+        <div className='flex flex-2 grow items-center flex-wrap mt-3 -m-3 '>
+          {projects.map((project) => (
+            <div className='w-1/3 p-3' key={project.id}>
+              <Link href={`/project/${project.id}`}>
+                <ProjectCard project={project} />
+              </Link>
+            </div>
+          ))}
+          <div className='w-1/3 p-3'>
+            <NewProject />
+          </div>
+        </div>
+        <div className='mt-6 flex-2 grow w-full flex'>
+          <div className='w-full'>
+            <TaskCard />
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,15 +3,16 @@ import Sidebar from '@/components/Sidebar';
 // import '@styles/global.css';
 import '../../styles/global.css';
 
-export default function DashBoardRootLayout({ children }:any) {
+export default function DashBoardRootLayout({ children }: any) {
   return (
     <html lang='en'>
       <head />
       <body className='h-screen w-screen candy-mesh p-6'>
         <GlassPane className='w-full h-full flex items-center'>
-          <Sidebar/>
+          <Sidebar />
           {children}
         </GlassPane>
+        <div id='modal'></div>
       </body>
     </html>
   );
